f23fafb873765451eda278c57e31f497a9a43c1b
algebraicdianuj <dianujkotov1997@gmail.com>
2025-09-05 02:59:35 -0500

    cleaning things, and specific configurations of training

---
+++ main_hf_trladv.py
@@ -0,0 +1,32 @@
+from model.model_hf import *
+from dataset.csqa import load_dataset
+from utils.param_counter import *
+from train.trl_trainer_adv import *
+import yaml
+
+
+def main():
+    # Path to your config file
+    config_path = "config.yaml"
+
+    # Open the config file and load its contents
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file) # Use safe_load for security
+
+    # Load the model and tokenizer
+    model, tokenizer = get_model_stuff(config, do_lora=False)
+
+    # Load and preprocess the dataset
+    dataset = load_dataset(tokenizer, split='train')
+
+    # Train the model using TRL's SFTTrainer
+    train_with_trl(model, tokenizer, dataset, config, do_instrument=True)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ main_unsloth_torchadv.py
@@ -0,0 +1,32 @@
+from model.model_unsloth import *
+from dataset.csqa import load_dataset
+from utils.param_counter import *
+from train.torch_trainer_adv import *
+import yaml
+
+
+def main():
+    # Path to your config file
+    config_path = "config.yaml"
+
+    # Open the config file and load its contents
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file) # Use safe_load for security
+
+    # Load the model and tokenizer
+    model, tokenizer = get_model_stuff(config, do_lora=False)
+
+    # Load and preprocess the dataset
+    dataset = load_dataset(tokenizer, split='train')
+
+    # Train the model using TRL's SFTTrainer
+    train_with_torch(model, tokenizer, dataset, config, do_instrument=True)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
